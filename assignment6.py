d76114404af6aec543e6baa945d30ff4f40c5bb9
n-east <neast@student.waubonsee.edu>
2019-03-05 11:00:16 -0600

    Began basic program that compares password with user input

---
+++ assignment6.py
@@ -0,0 +1,6 @@
+correct_pass = "password123"
+user_pass = input("Please enter your password: ")
+if user_pass == correct_pass:
+    print("Correct! Password accepted.")
+else:
+    print("Incorrect.")
