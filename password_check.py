d22873f808549ac49ee4ab54473b4d679796fef0
n-east <neast@student.waubonsee.edu>
2019-03-05 11:23:32 -0600

    Renamed file to 'password_check.py'

---
+++ password_check.py
@@ -0,0 +1,11 @@
+attempt_number = 0
+correct_pass = "password123"
+user_pass = input("Please enter your password: ")
+if user_pass == correct_pass:
+    print("Correct! Password accepted.")
+else:
+    while user_pass != correct_pass:
+        print("Incorrect.")
+        attempt_number += 1
+        print("Password attempt number: ", attempt_number)
+        user_pass == input("Please enter your password: ")
